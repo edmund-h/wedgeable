58cea844c5e57602c84d83a9c404552322f8ef0e
edmund-h <447ght@gmail.com>
2017-05-05 19:21:33 -0400

    ProfileView infoViews no longer selectable.

--- wedgeable/wedgeable/ProfileViewController.swift
+++ wedgeable/wedgeable/ProfileViewController.swift
@@ -46,6 +46,10 @@ class ProfileViewController: UITableViewController {
         }
     }
     
+    override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
